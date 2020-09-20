67fbf2bfd7e1df8fb1428fdd2703911521382e49
Mac User <macuser@macs-mbp.home>
2020-09-20 00:17:52 -0400

    first commit

--- src/Comp/App.js
+++ src/Comp/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import $ from 'jquery'
 import firebase from 'firebase'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {Redirect} from 'react-router';
 // impoer app,base from 'rebase'
 import '../Css/App.css';
 
